6320b7fc2746c9bd45afa79848ee0fab0f9fc7df
hppod <hppodgornik@gmail.com>
2019-05-25 00:28:22 -0300

    Implement user routes

---
+++ app/routes/user.js
@@ -0,0 +1,22 @@
+//User table routes
+/*
+    GET BY ID
+    PUT
+    DELETE
+
+    GET BY ID WITH REVIEW
+
+    CREATE USER
+    LOGIN
+*/
+
+const express = require('express')
+const route = express.Router()
+const UserController = require('./../controllers/user')
+
+route.get('/user/:id', UserController.getUserById)
+route.put('/user/:id', UserController.updateUser)
+route.delete('/user/:id', UserController.destroyUser)
+route.post('/user', UserController.createUser)
+
+module.exports = route
