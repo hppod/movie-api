531c3a5718813bc1752f2aac62fde0eccde89a9f
hppod <hppodgornik@gmail.com>
2019-05-24 22:18:30 -0300

    Create director model

---
+++ app/models/director.js
@@ -0,0 +1,36 @@
+//Creating model director table
+/*
+    ID INT NOT NULL PK_ACTOR AI
+    NAME VARCHAR(100) NOT NULL
+    BORNDATE DATE NOT NULL
+*/
+
+module.exports = (sequelize, DataTypes) => {
+    const DirectorModel = sequelize.define('DirectorModel', {
+
+        ID: {
+            type: DataTypes.INTEGER,
+            required: true,
+            primaryKey: true,
+            autoIncrement: true,
+            allowNull: false
+        },
+        NAME: {
+            type: DataTypes.STRING,
+            required: true,
+            max: 100,
+            allowNull: false
+        },
+        BORNDATE: {
+            type: DataTypes.DATE,
+            required: true,
+            allowNull: false
+        }
+    },
+        {
+            tableName: 'DIRECTOR',
+            timestamps: false
+        }
+    )
+    return DirectorModel
+}
