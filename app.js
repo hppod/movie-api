23a118d59b8ad032b1d97211b0d7b23c6686f5c4
hppod <hppodgornik@gmail.com>
2019-06-03 12:42:24 -0300

    add app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
+const app = express()
 const bodyParser = require('body-parser')
 const cors = require('cors')
-const app = express()
 const port = process.env.PORT || 3000
 
 app.use(cors())
@@ -14,14 +14,26 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: 'application/json' }));
 
-const filmeRoute = require('./app/app.routes')
-
-app.use('/', filmeRoute)
+const ActorRoutes = require('./app/routes/actor')
+const DirectorRoutes = require('./app/routes/director')
+const MovieRoutes = require('./app/routes/movie')
+const RRRoutes = require('./app/routes/rr')
+const UserRoutes = require('./app/routes/user')
+const WriterRoutes = require('./app/routes/writer')
 
 app.get('/', function (req, res) {
-    res.send(`API Server online - port ${port}`)
+    res.send('api works')
 })
 
-app.listen(port, function (req, res) {
-    console.log(`Umbler app listening on port ${port}`)
+app.use('/', ActorRoutes)
+app.use('/', DirectorRoutes)
+app.use('/', MovieRoutes)
+app.use('/', RRRoutes)
+app.use('/', UserRoutes)
+app.use('/', WriterRoutes)
+
+app.listen(port, function () {
+    console.log(`api works on port ${port}`)
 })
+
+module.exports = app
