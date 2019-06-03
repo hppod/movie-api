0c310bbe354f5d61d1c83c0047de029e51ecf783
hppod <hppodgornik@gmail.com>
2019-06-03 19:31:52 -0300

    clear repository

--- app/app.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const route = express.Router()
-const FilmeController = require('./filme')
-
-route.get('/filmes', FilmeController.get)
-route.post('/filme', FilmeController.post)
-route.delete('/filme/:id', FilmeController.delete)
-
-module.exports = route
